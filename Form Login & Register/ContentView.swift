2cac196313332b54917c46068b02b7ac7007486f
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-11-01 06:01:34 +0700

    Initial Commit

---
+++ Form Login & Register/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Form Login & Register
+//
+//  Created by Yusuf Saifudin on 01/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
